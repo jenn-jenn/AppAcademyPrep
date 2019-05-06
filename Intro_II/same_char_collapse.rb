55e39c3477f5cdb7b7c450d656b9307b621b7829
jenn-jenn <jenn-jenn@users.noreply.github.com>
2019-05-05 20:15:57 -0700

    last exercise to add

--- Intro_II/same_char_collapse.rb
+++ Intro_II/same_char_collapse.rb
@@ -1,3 +1,10 @@
+# Write a method same_char_collapse that takes in a string and returns a
+# collapsed version of the string. To collapse the string, we repeatedly
+# delete 2 adjacent characters that are the same until there are no such
+# adjacent characters. If there are multiple pairs that can be collapsed,
+# delete the leftmost pair. For example, we take the following steps to
+# collapse "zzzxaaxy": zzzxaaxy -> zxaaxy -> zxxy -> zydef same_char_collapse(str)
+
 def same_char_collapse(str)
   i = 0
   while i < str.length
